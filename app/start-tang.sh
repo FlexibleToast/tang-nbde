e64f36d10c74a120fadc7e7b4792c1a3eaa3982a
FlexibleToast <JMcDade42@gmail.com>
2022-09-29 17:16:59 -0500

    Leave the directory alone

--- app/start-tang.sh
+++ app/start-tang.sh
@@ -4,8 +4,4 @@ set -e
 
 mkdir -p /var/db/tang /var/cache/tang
 
-if [ -z "${ENTRYPOINT_RUN_AS_ROOT:-}" ]; then
-    chown -R ${PUID}:${GUID} /var/db/tang
-fi
-
 socat tcp-l:80,reuseaddr,fork exec:"/usr/libexec/tangd /var/cache/tang"
