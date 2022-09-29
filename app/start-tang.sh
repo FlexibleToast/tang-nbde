68970f7617559e3351452d486889cb484f9413cf
FlexibleToast <JMcDade42@gmail.com>
2022-09-29 17:10:19 -0500

    Change chown

--- app/start-tang.sh
+++ app/start-tang.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
-if [ -z "${ENTRYPOINT_RUN_AS_ROOT:-}" ]; then
-    chown -R $PUID:$GUID /var/db/tang
-fi
-
 mkdir -p /var/db/tang /var/cache/tang
+
+if [ -z "${ENTRYPOINT_RUN_AS_ROOT:-}" ]; then
+    chown -R ${PUID}:${GUID} /var/db/tang
+fi
 
 socat tcp-l:80,reuseaddr,fork exec:"/usr/libexec/tangd /var/cache/tang"
